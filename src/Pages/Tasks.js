f50cd5f52c4d8272904269c7a036dae2a61d64da
FarhanZizz <iamfarhanaziz@gmail.com>
2023-06-05 21:11:22 +0600

    [Bug Fix] Where task would'nt show after relode

--- src/Pages/Tasks.js
+++ src/Pages/Tasks.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useState, useEffect } from "react"
 import { AuthContext } from "../AuthProvider/AuthProvider"
 import TaskStat from "../Components/TaskStat"
 import Task from "../Components/Task"
@@ -6,18 +6,21 @@ import { useQuery } from "react-query"
 
 const Tasks = () => {
   const { user } = useContext(AuthContext)
+  const [sortValue, setSortValue] = useState("")
+
   const { data: tasks = [], refetch } = useQuery({
-    queryKey: ["tasks"],
+    queryKey: ["tasks", user?.uid], // Include user ID in the query key
     queryFn: async () => {
-      const res = await fetch(
-        `https://mcs-task-management-server.vercel.app/tasks/${user?.uid}`
-      )
-      const data = await res.json()
-      return data
+      if (user) {
+        const res = await fetch(
+          `https://mcs-task-management-server.vercel.app/tasks/${user.uid}`
+        )
+        const data = await res.json()
+        return data
+      }
+      return []
     },
   })
-
-  const [sortValue, setSortValue] = useState("")
 
   const handleSortChange = (e) => {
     setSortValue(e.target.value)
@@ -39,6 +42,10 @@ const Tasks = () => {
   }
 
   const sortedTasks = sortTasks()
+
+  useEffect(() => {
+    refetch() // Refetch tasks whenever the user ID changes
+  }, [user?.uid, refetch])
 
   return (
     <div className="grid gap-10 justify-center mt-5">
